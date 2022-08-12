7bd16c3dfed29209d7a2c24d215f614690bdce62
Niall Colman <niallcolman@Nialls-MacBook-Pro.local>
2022-08-12 23:22:21 +0100

    basic styling and background javascript added

---
+++ backgroundChanger.js
@@ -0,0 +1,21 @@
+const bodyTag = document.querySelector("body");
+
+let imageGallery = [
+  "url(./images/williams-joinery-2.jpg)",
+  "url(./images/williams-joinery-1.jpg)",
+  "url(./images/williams-joinery-3.jpg)",
+  "url(./images/williams-joinery-4.jpg)",
+  "url(./images/williams-joinery-6.jpg)",
+  "url(./images/williams-joinery-7.jpg)",
+  "url(./images/williams-joinery-8.jpg)",
+  "url(./images/williams-joinery-9.jpg)",
+  "url(./images/williams-joinery-10.jpg)",
+  "url(./images/williams-joinery-11.jpg)",
+  "url(./images/williams-joinery-12.jpg)",
+];
+
+let counter = 0;
+
+bodyTag.addEventListener("click", function () {
+  bodyTag.style.backgroundImage = imageGallery[counter++];
+});
---
+++ information.js
@@ -0,0 +1,7 @@
+function openInfo() {
+  document.getElementById("myNav").style.width = "30%";
+}
+
+function closeInfo() {
+  document.getElementById("myNav").style.width = "0%";
+}
